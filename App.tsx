80ea2d67b6cccbf4f1393a5bb088606a62a082ce
Kevin Gracia Orejuela <kevin.gracia@ppm.com.ec>
2022-03-20 22:32:06 -0500

    feat: tabs navigation

--- App.tsx
+++ App.tsx
@@ -4,16 +4,44 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/HomeScreen';
 import ExploreScreen from './src/screens/ExploreScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import {Platform} from 'react-native';
+import SettingsScreen from './src/screens/SettingsScreen';
+import colors from './src/styles/colors';
 
 // import {ENV_VAR} from '@env';
 // import {API_EV} from '@env';
 
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: colors.orange,
+    background: colors.darkBlue,
+    card: 'rgb(255, 255, 255)',
+    text: colors.gray,
+    border: colors.darkBlue,
+    notification: colors.orange,
+  },
+};
 const App = () => {
   const Tab = createBottomTabNavigator();
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
+    <NavigationContainer theme={MyTheme}>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+            height: 90,
+            paddingHorizontal: 5,
+            paddingTop: 0,
+            backgroundColor: colors.darkBlue,
+            position: 'absolute',
+            borderTopWidth: 0,
+          },
+        }}>
         <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
         <Tab.Screen name="Explore" component={ExploreScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+const colors = {
+  darkBlue: '#140E36',
+  orange: '#FF9D2C',
+  gray: '#000000',
+};
+
+export default colors;
